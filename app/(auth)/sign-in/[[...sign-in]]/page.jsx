6f90be5ac1131beb59a0b2e35648cd576f291893
Munirah Anjum <munirahfariha88@gmail.com>
2024-10-10 13:41:00 +0600

    Update page.jsx

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -29,7 +29,7 @@ export default function Page() {
           </a>
   
           <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
-            Welcome to Squid 🦑
+            Welcome to Industrial Expo
           </h2>
   
           <p className="mt-4 leading-relaxed text-white/90">
@@ -63,7 +63,7 @@ export default function Page() {
             </a>
   
             <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
-              Welcome to Squid 🦑
+              Welcome to Industrial
             </h1>
   
             <p className="mt-4 leading-relaxed text-gray-500">
